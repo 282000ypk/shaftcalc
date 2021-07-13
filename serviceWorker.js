4a7ab75e9e085c57ad34f43cf26287538f3e5b24
Yogesh Kamble <66812707+282000ypk@users.noreply.github.com>
2021-07-13 15:04:56 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -8,9 +8,7 @@ self.addEventListener('activate', function(event){
 
 var STATIC_CACHE_CONTAINER = "static_v1"
 var STATIC_FILES = [
-    "./index.html",
-    "./index.js",
-    "./index.css"
+    "./index.html"
 ]
 
 self.addEventListener('install', function(event){
