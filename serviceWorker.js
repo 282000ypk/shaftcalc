2811ab4a65163e8049e5a109b18098b68027ac4a
yogesh <ypk282000@gmail.com>
2021-07-17 16:27:17 +0530

    servicewroker recretaed

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,23 +1,16 @@
+function registerServiceWorker(){
+    navigator.serviceWorker
+    .register('/serviceWorker.js')
+    .then(function(reg){
+        console.log("service worker registered", reg)
+    })
+    .catch(function(err){
+        console.log("error when registering service worker", err)
+    })
+}
+
 self.addEventListener('install', function(event){
     console.log("service worker installed", event)
-})
-
-self.addEventListener('activate', function(event){
-    console.log("service worker activated", event)
-})
-
-var STATIC_CACHE_CONTAINER = "static_v1"
-var STATIC_FILES = [
-    "./index.html"
-]
-
-self.addEventListener('install', function(event){
-    event.waitUntil(
-        caches.open(STATIC_CACHE_CONTAINER)
-        .then(function(cache){
-            cache.addAll(STATIC_FILES)
-        })
-    )
 })
 
 self.addEventListener('activate', function(event){
